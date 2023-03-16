40da5210c5eef17808fd169f9a425c17633d60af
aidanaN <nugumetova3001@gmail.com>
2023-03-16 10:05:04 -0500

    google commit welcome

--- src/test/java/test/Google.java
+++ src/test/java/test/Google.java
@@ -5,4 +5,7 @@ public class Google {
     public static void main(String[] args) {
         System.out.println("Hello google");
     }
+    public void welcome(){
+        System.out.println("Welcome to the city");
+    }
 }
